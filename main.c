e5e54e854ce5c882ce13255d2cccd57b3adf2ebb
Eurus001 <86990592+Eurus001@users.noreply.github.com>
2023-06-05 16:23:41 -0500

    Segundo Commit, Agregadas las funciones

--- main.c
+++ main.c
@@ -43,31 +43,31 @@ int Menu(float Alumnos[N_ALUM][N_PROG], int flag){
 
     int menu, resultado;
 
-    resultado = scanf("%d", menu);
+    printf("Elija una opcion: \n1. Obtener el promedio de calificaciones de cada alumno durante el semestre \n2. La nota promedio del grupo de estudiantes para cada progreso \n");
+    printf("3. El alumno que obtuvo el mayor promedio de calificación durante el semestre \n4. Salir\n");
+    resultado = scanf("%d", &menu);
 
-    while (resultado != 1)
-    {
+    //while (resultado != 1)
+    //{
         
-    }
+    //}
     
 
     switch (menu){
     case 1:
-    
+        PromedioAlumno(Alumnos);
         break;
     case 2:
-    
+        PromedioProgreso(Alumnos);
         break;
-    
     case 3:
-    
+        MejorNota(Alumnos);
         break;
-
     case 4:
-    
+        flag = 0;
         break;
-
     default:
+        printf("Ingrese una opcion valida");
         break;
     }
 
@@ -91,5 +91,60 @@ void ImprimirMatriz(float Alumnos[N_ALUM][N_PROG]){
     }
     
     printf("\n");
+
+}
+
+void PromedioAlumno(float Alumnos[N_ALUM][N_PROG]){
+    float suma = 0, promedio = 0;
+    int alumnoA = 0;
+
+    printf("Elija el alumno: ");
+    scanf("%d", &alumnoA);
+
+    for (int i = 0; i < N_PROG; i++)
+    {
+        suma = suma + Alumnos[alumnoA-1][i];
+    }
+
+    promedio = suma / N_PROG;
+
+    printf("El promedio del alumno %d es %.2f\n", alumnoA, promedio);
+}
+
+void PromedioProgreso(float Alumnos[N_ALUM][N_PROG]){
+    float suma, promedio;
+    int progresoA;
+
+    printf("Elija el progreso: ");
+    scanf("%d", &progresoA);
+
+    for (int i = 0; i < N_ALUM; i++)
+    {
+        suma = suma + Alumnos[progresoA-1][i];
+    }
+
+    promedio = suma / N_ALUM;
+
+    printf("El promedio del progreso %d es %.2f\n", progresoA, promedio);
+
+}
+
+void MejorNota(float Alumnos[N_ALUM][N_PROG]){
+    float max = 0;
+    int maxI = 0, maxJ = 0;
+
+    for (int i = 0; i < N_ALUM; i++)
+    {
+        for (int j = 0; j < N_PROG; j++)
+        {
+            if (Alumnos[i][j] > max)
+            {
+                max = Alumnos[i][j];
+                maxI = i;
+                maxJ = j;
+            }          
+        }
+    }
+    
 
 }
